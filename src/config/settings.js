be7247fb8499b4b91cd535b214f23e4ea22b5991
Willian Antunes <willian.lima.antunes@gmail.com>
2021-04-20 13:56:54 -0300

    fix: env variables can be read from the client-side

--- src/config/settings.js
+++ src/config/settings.js
@@ -17,14 +17,33 @@ function evalEnvAsBoolean(envValue, standardValue = null) {
   return trueValues.includes(valueAsLowerCase)
 }
 
-const SITE_URL = getEnvOrRaiseException("SITE_URL", process.env.SITE_URL)
+function findValue(v1, v2) {
+  return [v1, v2].filter(v => v)[0]
+}
 
-const GOOGLE_TAG_MANAGER_ID = getEnvOrRaiseException("GOOGLE_TAG_MANAGER_ID", process.env.GOOGLE_TAG_MANAGER_ID)
-const GTM_INCLUDE_DEVELOPMENT = evalEnvAsBoolean(process.env.GTM_INCLUDE_DEVELOPMENT, false)
+const SITE_URL = getEnvOrRaiseException("SITE_URL", findValue(process.env.SITE_URL, process.env.GATSBY_SITE_URL))
 
-const NETLIFY_CMS_LOCAL_BACKEND = evalEnvAsBoolean(process.env.NETLIFY_CMS_LOCAL_BACKEND, false)
-const NETLIFY_CMS_BACKEND_REPO = getEnvOrRaiseException("NETLIFY_CMS_BACKEND_REPO", process.env.NETLIFY_CMS_BACKEND_REPO)
-const NETLIFY_CMS_BACKEND_BRANCH = getEnvOrRaiseException("NETLIFY_CMS_BACKEND_BRANCH", process.env.NETLIFY_CMS_BACKEND_BRANCH)
+const GOOGLE_TAG_MANAGER_ID = getEnvOrRaiseException(
+  "GOOGLE_TAG_MANAGER_ID",
+  findValue(process.env.GOOGLE_TAG_MANAGER_ID, process.env.GATSBY_GOOGLE_TAG_MANAGER_ID)
+)
+const GTM_INCLUDE_DEVELOPMENT = evalEnvAsBoolean(
+  findValue(process.env.GTM_INCLUDE_DEVELOPMENT, process.env.GATSBY_GTM_INCLUDE_DEVELOPMENT),
+  false
+)
+
+const NETLIFY_CMS_LOCAL_BACKEND = evalEnvAsBoolean(
+  findValue(process.env.NETLIFY_CMS_LOCAL_BACKEND, process.env.GATSBY_NETLIFY_CMS_LOCAL_BACKEND),
+  false
+)
+const NETLIFY_CMS_BACKEND_REPO = getEnvOrRaiseException(
+  "NETLIFY_CMS_BACKEND_REPO",
+  findValue(process.env.NETLIFY_CMS_BACKEND_REPO, process.env.GATSBY_NETLIFY_CMS_BACKEND_REPO)
+)
+const NETLIFY_CMS_BACKEND_BRANCH = getEnvOrRaiseException(
+  "NETLIFY_CMS_BACKEND_BRANCH",
+  findValue(process.env.NETLIFY_CMS_BACKEND_BRANCH, process.env.GATSBY_NETLIFY_CMS_BACKEND_BRANCH)
+)
 
 module.exports = {
   SITE_URL,
