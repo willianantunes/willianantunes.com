60100833a95098b9945265e11690c0e73a0f71e6
Willian Antunes <willian.lima.antunes@gmail.com>
2021-05-17 10:19:49 -0300

    chore: unneeded import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,4 @@
 import "prismjs/themes/prism-tomorrow.css"
-import React from "react"
 import { wrapWithDarkThemeProvider } from "./src/contexts/dark-theme-context"
 
 export const wrapRootElement = wrapWithDarkThemeProvider
